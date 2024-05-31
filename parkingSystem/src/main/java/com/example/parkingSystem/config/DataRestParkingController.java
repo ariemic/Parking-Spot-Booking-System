a1d0bc7cb246f07b3c5242ddac8bb2acbb6cc7b0
Ariel <arie.michalik@gmail.com>
2024-05-31 15:17:18 +0200

    numberOfFreeSlots + endpointy
    1. Funkcja numberOfFreeSltos w parkingService
    2. endpoity na sprawdzanie liczby wolnych miejsc na danych parkingu dzis i konkretnego dnia

--- parkingSystem/src/main/java/com/example/parkingSystem/config/DataRestParkingController.java
+++ parkingSystem/src/main/java/com/example/parkingSystem/config/DataRestParkingController.java
@@ -4,6 +4,7 @@ package com.example.parkingSystem.config;
 import com.example.parkingSystem.dao.BookingRepository;
 import com.example.parkingSystem.dao.ParkingRepository;
 import com.example.parkingSystem.entity.Parking;
+import com.example.parkingSystem.exceptions.ParkingNotFoundException;
 import com.example.parkingSystem.services.ParkingService;
 import com.example.parkingSystem.validation.DateValidation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,17 +43,45 @@ public class DataRestParkingController {
 
     @GetMapping("/getAllAvailableParkings/{date}")
     public ResponseEntity<?> getAllAvailableParking(@PathVariable String date) {
+        // ------------------------------------------------------------------
         DateValidation dateValidation = DateValidation.getInstance();
         if (!dateValidation.validateDate(date)) {
             return new ResponseEntity<>("Niepoprawny format daty", HttpStatus.BAD_REQUEST);
         }
+        // ------------------------------------------------------------------
 
         List<Parking> availableParkings = parkingService.listAvailableParkings(date);
         return new ResponseEntity<>(availableParkings, HttpStatus.OK);
     }
 
+    //todo dodanie przykladow do bazy i sprawdzenie czy logika działą
+    @GetMapping("/getNumberOfFreeSlotsToday/{parkingId}")
+    public ResponseEntity<?> getNumberOfFreeSlots(@PathVariable Integer parkingId){
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = today.format(formatter);
+        try {
+            if (!parkingService.parkingExist(parkingId)) {
+                throw new ParkingNotFoundException();
+            }
+            return new ResponseEntity<>(parkingService.numberOfFreeSlots(parkingId, formattedDate), HttpStatus.OK);
+        } catch (ParkingNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Wystąpił nieoczekiwany błąd");
+        }
 
-
+    }
+    @GetMapping("/getNumberOfFreeSlots/{parkingId}/{date}")
+    public ResponseEntity<?> getNumberOfFreeSlots(@PathVariable Integer parkingId, @PathVariable String date){
+        // ------------------------------------------------------------------
+        DateValidation dateValidation = DateValidation.getInstance();
+        if (!dateValidation.validateDate(date)) {
+            return new ResponseEntity<>("Niepoprawny format daty", HttpStatus.BAD_REQUEST);
+        }
+        // ------------------------------------------------------------------
+        return new ResponseEntity<>(parkingService.numberOfFreeSlots(parkingId, date), HttpStatus.OK);
+    }
 
 
 
--- parkingSystem/src/main/java/com/example/parkingSystem/services/ParkingService.java
+++ parkingSystem/src/main/java/com/example/parkingSystem/services/ParkingService.java
@@ -32,23 +32,29 @@ public class ParkingService {
     }
 
 
-    public List<Parking> listAvailableParkings(String date){
+    private Map<Parking, Integer> calculateBookedSlots(String date){
         List<Parking> parkings = parkingRepository.findAll();
         List<Booking> bookings = bookingRepository.findAll();
+        //przechowuje liczbe <Parking, liczba zajetych miejsc>
         Map<Parking, Integer> parkingMap = new HashMap<>();
-
-        for (Parking parking : parkings) {
+        for (Parking parking: parkings){
             parkingMap.put(parking, 0);
         }
-        for (Booking booking : bookings) {
+        for(Booking booking: bookings){
             if(booking.getBookingDate().equals(date)){
                 int parkingId = booking.getParkingId();
                 Parking bookedParking = parkingRepository.findByParkingId(parkingId);
                 parkingMap.put(bookedParking, parkingMap.get(bookedParking) + 1);
             }
-
         }
+        return parkingMap;
+
+    }
+
+    public List<Parking> listAvailableParkings(String date){
+        Map<Parking, Integer> parkingMap = calculateBookedSlots(date);
         List<Parking> availableParkingsNow = new ArrayList<>();
+
         for(Map.Entry<Parking, Integer> entry: parkingMap.entrySet()){
             Parking potentialParking = entry.getKey();
             int bookedSlots = entry.getValue();
@@ -59,4 +65,13 @@ public class ParkingService {
         return availableParkingsNow;
 
     }
+
+    public int numberOfFreeSlots(int parkingId, String date){
+        Map<Parking, Integer> parkingMap = calculateBookedSlots(date);
+        Parking parking = parkingRepository.findByParkingId(parkingId);
+        int bookedSlots = parkingMap.get(parking);
+        return parking.getMaxSlots() - bookedSlots;
+    }
+
+
 }
