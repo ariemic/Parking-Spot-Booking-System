f96bac93b18617e9dfd11039eab8626682690e69
Ariel <arie.michalik@gmail.com>
2024-06-02 18:31:25 +0200

    remove garbage

--- parkingSystem/src/main/java/com/example/parkingSystem/ParkingSystemApplication.java
+++ parkingSystem/src/main/java/com/example/parkingSystem/ParkingSystemApplication.java
@@ -1,11 +1,7 @@
 package com.example.parkingSystem;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class ParkingSystemApplication {
@@ -13,14 +9,5 @@ public class ParkingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingSystemApplication.class, args);
 	}
-//	@Bean
-//	public CommandLineRunner myCommandLineRunner() {
-//		return args -> {
-//			// Your MySQL query execution code goes here
-//			// For example:
-//			// myRepository.executeMySqlQuery();
-//		};
-//	}
-
 
 }
--- parkingSystem/src/main/java/com/example/parkingSystem/dao/SubscriberRepository.java
+++ parkingSystem/src/main/java/com/example/parkingSystem/dao/SubscriberRepository.java
@@ -1,9 +1,6 @@
 package com.example.parkingSystem.dao;
 
 import com.example.parkingSystem.entity.Subscriber;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
